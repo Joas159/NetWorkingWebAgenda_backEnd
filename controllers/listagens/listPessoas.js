798674688e371e4b20662467b97235825426380b
Joas159 <joassousaesilva@gmail.com>
2022-05-17 13:58:26 -0300

    implementacao listagem pessoas

--- controllers/listagens/listPessoas.js
+++ controllers/listagens/listPessoas.js
@@ -1,7 +1,24 @@
 const conexao = require("../../src/connections");
+const jwt = require("jsonwebtoken");
+const secretKey = require("../../src/passPhraseJwt");
 
 const ctrl_listPessoas = async (req, res) => {
-    return res.status(200).json('teste de conexão da listagem de pessoas');
+    const { email, token } = req.body;
+
+    if (!email || !token) {
+        return res.status(400).json({"Mensagem": "parâmetro obrigatório não informado"});
+    }
+
+    try {
+        const usuario = jwt.verify(token, secretKey);   
+        console.log(`${usuario.email} está realizando tentativa de acesso`);
+    } catch (error) {
+        res.status(403).json({"Mensagem": "Sessão Encerrada. Realizar Login Novamente"});
+    }
+   
+    //Resultado Geral
+    const listaGeral = await conexao.query('select * from pessoas');
+    return res.json(listaGeral.rows);
 }
 
 module.exports = {
